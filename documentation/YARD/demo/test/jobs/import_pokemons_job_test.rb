2561d42364f564655fe343511b0c447cf7dafbf0
Carlos Medrano <cmedrano@applaudostudios.com>
2021-04-18 19:45:54 -0600

    :construction: Add some doc on test

--- documentation/YARD/demo/test/jobs/import_pokemons_job_test.rb
+++ documentation/YARD/demo/test/jobs/import_pokemons_job_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class ImportPokemonsJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # @!method Test - Should create pokemons
+  #  Use the {file:test/fixtures/files/import_pokemons_test.csv import_pokemon_test.csv} file to test the {#ImportPokemonsJob importPokemonsJob}
+  
+  test 'Should create Pokemons' do
+    ImportPokemonsJob.perform_now(Rails.root.join('test/fixtures/files/import_pokemons_test.csv'))
+    assert Pokemon.count, 9
+  end
 end
