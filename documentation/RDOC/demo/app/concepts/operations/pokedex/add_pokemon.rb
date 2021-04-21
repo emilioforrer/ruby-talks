793a61a24be06488b46156b6ebd93b21f9917e67
Carlos Medrano <cmedrano@applaudostudios.com>
2021-04-21 13:46:46 -0600

    :sparkles: Add RDOC documentation

--- documentation/RDOC/demo/app/concepts/operations/pokedex/add_pokemon.rb
+++ documentation/RDOC/demo/app/concepts/operations/pokedex/add_pokemon.rb
@@ -1,6 +1,14 @@
 module Operations
   module Pokedex
-    # Operation to add pokemon to pokedex
+    # = Operation to add pokemon to pokedex
+    # == validate_pokemon!
+    # Method used to validate if pokemon exist
+    # === error!
+    # Raised using Paw::Utils
+    # == validate_pokedex_size!
+    # Method used to valid if pokemon exist
+    # == persist!
+    # Method used to save data
     class AddPokemon < Trailblazer::Operation
       delegate :error!, to: Paw::Utils
       pass :validate_pokemon!
@@ -9,12 +17,20 @@ module Operations
       pass :present!
 
       # Method used to valid if pokemon exist
+      # [find] Find pokemon by params on route
+      # [error!] Raise error if pokemon is not found
       def validate_pokemon!(options, params:, **)
         options[:model] = Pokemon.find_by(id: params[:pokemon_id])
         error!(status: 404, message: I18n.t('paw.api.messages.not_found')) if options[:model].nil?
       end
 
-      # Method used to validate if the limit of the pokedex has been reached
+      ##
+      # Method used to validate if the limit of the pokedex has been reached <br>
+      #--
+      # This is a internal comment
+      #++
+      # 1. raise ::Paw::Exceptions::StandardError if the User reach the maximum of pokemons allowed
+      # 2. raise ::Paw::Exceptions::StandardError if the User reach the maximum of pokemons allowed
       def validate_pokedex_size!(options, current_user:, **)
         options[:pokedex] = current_user.pokemon_ids
         error!(status: 403, message: 'User reached the maximum of pokemons allowed') if options[:pokedex].size >= 5
@@ -26,6 +42,8 @@ module Operations
       end
 
       # Method used to retrieve information to user
+      # * First create de data
+      # * Then pass data to json
       def present!(options, **)
         data = { data: { message: 'Pokemon added to pokedex', status: 200 } }
         options[:response] = data.to_json
